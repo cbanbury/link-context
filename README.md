58609fbc9d7574364ac73d3d56f50530ebf844f3
Carl Banbury <carl@googolmail.co.uk>
2014-11-28 19:40:27 +0000

    Added README and a real database

---
+++ README.md
@@ -0,0 +1,90 @@
+# Short URLs with context
+A simple server to generate unique short URLs and attach some contextual data to them.
+
+A live version will soon be up on http://asc.li for testing purposes.
+
+
+## Usage
+
+### Create a new short link `GET /create/new/link/`
+
+    http://localhost/create/new/link/?context=`CONTEXT`&redirect=`REDIRECT`
+
+`CONTEXT` can be any arbitrary string and is the data we want to attach to the link we are about to create.
+
+`REDIRECT` where do you want the short link to go when clicked on.
+
+
+Example:
+
+    http://asc.li/create/new/link?context=bob&redirect=http://ubunutu.com
+
+    response --> http://asc.li/obdkf21
+
+This attaches the data `bob` to our link and will redirect to `http://ubuntu.com` when someone clicks on the new `http://asc.li/obdkf21` link.
+
+### Getting link statistics
+
+#### Find by specific short URL `GET /link/`
+
+    http://localhost/link/?path=`PATH`
+
+`PATH` The short url we want statistics for.
+
+Example:
+
+    http://asc.li/link/?path=http://asc.li/obdkf21
+
+    response --> {
+      "context": "bob",
+      "clicks": 17,
+      "link": "http://asc.li/obdkf21",
+      "redirect": "http://ubuntu.com"
+    }
+
+Here we have retrieved the data from the example above. Currently the only additional data tracked is the number of `clicks` the link has had.
+
+#### Match on context `GET /link/context/`
+TODO: Ability to fetch data by matching against part of the context assigned to the link
+
+### Running Locally
+If you don't want to use http://asc.li and would rather run your own instance, its as easy as:
+
+    $ npm install context
+    $ cd node_modules/context
+    $ npm start
+
+Requirements:
+- node/npm ~ 0.10.33
+- MongoDB ~ 2.6.x
+
+In you want the server running constantly, bring it up with [naught](https://www.npmjs.org/package/naught):
+
+    $ naught start server.js
+
+Requirements:
+- naught installed globally
+
+##### CONFIG
+All config is currently stored in `server.js`. This and much of the code will be put somewhere more sensible in due course.
+
+<table>
+  <tr>
+    <td>Config Variable</td><td>Description</td>
+  </tr>
+  <tr>
+    <td>SEED</td><td>[shortid](https://www.npmjs.org/package/shortid) seed </td>
+  </tr>
+  <tr>
+    <td>PORT</td><td>Port the server will run on (default 8000)</td>
+  </tr>
+  <tr>
+    <td>DOMAIN</td><td></td>
+  </tr>
+</table>
+
+### Authentication
+
+There is currently no authentication supported. The aim is to make as easy as possible to test in a browser or with cURL. If your link context is not open data, just spin up your own instance and use a firewall to limit access to the API.
+
+TODO: Token based authentication coming soon to the http://asc.li so you won't need to run you own version ;)
--- server.js
+++ server.js
@@ -3,17 +3,31 @@ var Joi = require('joi');
 var shortId = require('shortid');
 var url = require('url');
 
-var SEED = 4598;
+
+// SECTION: Database configuration
+var MongoClient = require('mongodb').MongoClient;
+
+var _db;
+
+MongoClient.connect('mongodb://localhost:27017/asc', function (err, db) {
+  if (err) {
+    console.log('Error: Unable to connect to MongoDB');
+  }
+
+  _db = db.collection('links');
+});
+
+// SECTION: App config
+var SEED = 1;
 var PORT = 8000;
 var DOMAIN = 'localhost';
 var BASE_ROUTE = 'http://' + DOMAIN + ':' + PORT + '/';
 
+
+// SECTION: Main app
 shortId.seed(SEED);
 
 var server = new Hapi.Server(DOMAIN, PORT);
-
-// dummy database, replace with something like mongo/redis
-var database = [];
 
 // new link route
 server.route({
@@ -22,58 +36,63 @@ server.route({
   config: {
     validate: {
       query: {
-        uri: Joi.string().required(),
-        data: Joi.string().required()
+        redirect: Joi.string().required(),
+        context: Joi.string().required()
       }
     }
   },
   handler: function (request, reply) {
     var link = BASE_ROUTE + shortId.generate();
 
-    database.push({
+    _db.insert({
       link: link,
-      uri: JSON.parse(request.query.uri),
-      data: JSON.parse(request.query.data)
-    });
+      redirect: request.query.redirect,
+      context: request.query.context
+    }, function (err) {
+      if (err) {
+        return reply('Error: Unable to save link data.');
+      }
 
-    return reply(link);
+      return reply(link);
+    });
   }
 });
 
 // unique link and redirect route
 server.route({
   method: 'GET',
-  path: '/{id}', // TODO regex validation
-  handler: function (request, reply) {
-    var match = database.filter(function (item) {
-      return item.link === BASE_ROUTE + request.params.id;
-    });
-
-    if (match.length !== 1) {
-      return reply('Error: Sorry, no idea where to send you :(');
-    }
-
-    // increment clicks
-    match = match[0];
-
-    database.filter(function (item) {
-      if (item.link === BASE_ROUTE + request.params.id) {
-        if (item.clicks) {
-          item.clicks++;
-        } else {
-          item.clicks = 1;
+  path: '/{id}',
+  config: {
+      validate: {
+        params: {
+          id: Joi.string().regex(/[A-Za-z-_0-9]+/).required()
         }
       }
+  },
+  handler: function (request, reply) {
+    var search = BASE_ROUTE + request.params.id;
+
+    _db.findOne({link: search}, function (err, result) {
+      if (err) {
+        return reply(500);
+      }
+
+      if (!result) {
+        return reply('Error: link broken.');
+      }
+
+      if (result.clicks) {
+        result.clicks++;
+      } else {
+        result.clicks = 1;
+      }
+
+      return reply.redirect(url.format(result.redirect.toString()));
     });
-
-    console.log(database);
-
-    return reply.redirect(url.format(match.uri.toString()));
   }
 });
 
 // get by data route
-// TODO: should be JSONP
 server.route({
   method: 'GET',
   path: '/link/',
@@ -82,24 +101,33 @@ server.route({
       query: {
         path: Joi.string().required()
       }
-    }
+    },
+    jsonp: 'callback'
   },
   handler: function (request, reply) {
-    var match = database.filter(function (item) {
-      return item.link === request.query.path;
+    _db.findOne({link: request.query.path}, function (err, result) {
+      if (err) {
+        return reply(500);
+      }
+
+      if (!result) {
+        return reply('No context matching requested path.')
+      }
+
+      delete result._id;
+
+      return reply(result);
     });
+  }
+});
 
-    if (match.length === 0) {
-      return reply('Nothing found.');
-    }
-
-    if (match.length !== 1) {
-      return reply('Unable to uniquely identify link');
-    }
-
-    return reply(match[0]);
+process.on('message', function (message) {
+  if (message === 'shutdown') {
+    process.exit(0);
   }
 });
 
 
-server.start();
+server.start(function () {
+  if (process.send) process.send('online');
+});
